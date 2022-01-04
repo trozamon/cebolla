a757e2fbda7cf11454eb357999dfea01ca6e7269
Alec Ten Harmsel <alec@alectenharmsel.com>
2022-01-04 10:57:54 -0500

    wip - entities

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -1,6 +1,8 @@
 class Entity < ApplicationRecord
-  validates :name, presence: true
   has_one :address, as: :addressable
+  has_many :projects
+
+  validates :name, presence: true
 
   accepts_nested_attributes_for :address
 end
