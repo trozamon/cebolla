d2ca3160295ac47cc64ba8a3c10a63cff07bf3cb
Alec Ten Harmsel <alec@alectenharmsel.com>
2022-01-04 11:14:34 -0500

    wip - customers

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,9 +1,11 @@
 class Customer < ApplicationRecord
-  validates :name, presence: true
-
   has_many :projects
   has_many :tasks, through: :projects
   has_one :address, as: :addressable
+
+  validates :name, presence: true
+
+  accepts_nested_attributes_for :address
 
   monetize :default_hourly_rate_cents, allow_nil: true
 end
