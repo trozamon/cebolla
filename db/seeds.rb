fd8847b5c1201f98a672261666215070cb2ad7a7
Alec Ten Harmsel <alec@alectenharmsel.com>
2023-03-26 15:00:20 -0400

    Make seeds idempotent

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,27 +1,20 @@
 return unless Rails.env.development?
 
 ActiveRecord::Base.transaction do
-  sample = User.create!(email: 'sample@example.com',
-                        password: 'Password1!',
-                        password_confirmation: 'Password1!',
-                        confirmed_at: Time.zone.now)
+  sample = User.create_with(password: 'Password1!',
+                            password_confirmation: 'Password1!',
+                            confirmed_at: Time.zone.now)
+               .find_or_create_by!(email: 'sample@example.com')
 
-  entity = Entity.create!(
-    name: 'John Doe',
-    email: 'no-reply@example.com',
-    phone: '1234567890'
-  )
+  entity = Entity.create_with(email: 'no-reply@example.com',
+                              phone: '1234567890')
+                 .find_or_create_by!(name: 'John Doe')
 
-  customer = Customer.create!(
-    name: 'Customer 1, Inc.',
-    email: 'invoices@customer1.com',
-    default_hourly_rate: Money.new(10000, 'USD')
-  )
+  customer = Customer.create_with(email: 'invoices@customer1.com',
+                                  default_hourly_rate: Money.new(10000, 'USD'))
+                     .find_or_create_by!(name: 'Customer 1, Inc.')
 
-  project = Project.create!(
-    entity: entity,
-    customer: customer,
-    name: 'Default Project',
-    billing: :monthly
-  )
+  project = Project.create_with(name: 'Default Project',
+                                billing: :monthly)
+                   .find_or_create_by!(entity: entity, customer: customer)
 end
